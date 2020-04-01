9a25219d076e1c8ca3d765ddead7ce7da4796ff8
RexiousJV <54366507+RexiousJV@users.noreply.github.com>
2020-04-02 00:15:07 +0600

    Update Event.java

--- Event.java
+++ Event.java
@@ -11,30 +11,7 @@ public class Event {
         this.vanue = vanue;
     
     }
-    public String getname(String name)
-    {
-           return name;
-    }
-    public String getdate(String date)
-    {
-        return date;
-    }
-    public String getvanue(String vanue)
-    {
-        return vanue;
-    }
-    public void setname(String name)
-    {
-        this.name=name;
-    }
-        public void setdate(String date)
-        {
-            this.date=date;
-        }   
-        public void setvanue(String vanue)
-        {
-            this.vanue=vanue;
-        }
+   
     void display ()
     {
         System.out.println("Name : " +name);
