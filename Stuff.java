75f375cc9015ceb922d143ba8374dde50b6171a1
RexiousJV <54366507+RexiousJV@users.noreply.github.com>
2020-04-04 13:34:52 +0600

    Update Stuff.java

--- Stuff.java
+++ Stuff.java
@@ -1,25 +1,35 @@
 package EventManagmentSystem;
 
-public class Stuff extends Person {
+public class Stuff extends Person{
     private String ID;
-    private String dutytime;
+   private String dutytime;
 
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
-    public String getdutytime() {
-        return dutytime;
-    }
-
-    public void setdutytime(String dutytime) {
-        this.dutytime = dutytime;
-    }
-
-    void display() { 
-        System.out.println("ID:" +ID + "dutytime: "+dutytime);
+   public Stuff(String ID, String dutytime, String name, String email, String phone) {
+       super(name, email, phone);
+       this.ID = ID;
+       this.dutytime = dutytime;
    }
-    
-    
+
+   public String getID() {
+       return ID;
+   }
+
+   public void setID(String ID) {
+       this.ID = ID;
+   }
+
+   public String getDutytime() {
+       return dutytime;
+   }
+
+   public void setDutytime(String dutytime) {
+       this.dutytime = dutytime;
+   }
    
+   
+     void display() { 
+       System.out.println("ID:" +ID + "dutytime: "+dutytime);
+  }
+   
+
 }
