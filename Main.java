855f93129f834fbe77cc4ae3ea880c8bbcd96d04
RexiousJV <54366507+RexiousJV@users.noreply.github.com>
2020-04-04 13:33:31 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 package EventManagmentSystem;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         
+        manager manager = new manager("Melevalence Jam","Kidoo_lucu@gmail.com","012333312");   
+        Event event1 = new Event("E3","31 february","Darling point _Farmgate");
+        Expenditure obj = new  Expenditure("24 hrs",5000,"Madison Square Garden,LA.",1000,7000,event1);
+        
+        obj.display();
+ 
+        
     }
-    
 }
