9723faea6819e5c76d84020776bf8f2778a39b1f
RexiousJV <54366507+RexiousJV@users.noreply.github.com>
2020-04-04 13:31:59 +0600

    Update Expenditure.java

--- Expenditure.java
+++ Expenditure.java
@@ -1,65 +1,74 @@
 package EventManagmentSystem;
 
 public class Expenditure {
-	private String duration;
-	private float foodcost;
+    private String duration;
+private float foodcost;
     private String vanue;
     private float stuffcost;
     private float totalcost;
+    private Event event;
+    
 
-    public Expenditure (String duration, float foodcost,String vanue, float stuffcost, float totalcost) {
-    	this.duration=duration;
-        this.foodcost=foodcost;
-        this.vanue = vanue;
-        this.stuffcost=stuffcost;
-        this.totalcost=totalcost;
-    }
-    public String getduration(String duration)
-    {
-           return duration;
-    }
-    public float getdate(float foodcost)
-    {
-        return foodcost;
-    }
-    public String getvanue(String vanue)
-    {
-        return vanue;
-    }
-    public float getstuffcost1(float stuffcost)
-    {
-        return stuffcost;
-    }
-    public float getstuffcost(float totalcost)
-    {
-        return totalcost;
-    }
-    public void setduration(String duration)
-    {
-        this.duration=duration;
-    }
-        public void setfoodcost(float foodcost)
-        {
-            this.foodcost=foodcost;
-        }   
-        public void setvanue(String vanue)
-        {
-            this.vanue=vanue;
-        }
-        public void setstuffcost(float stuffcost)
-        {
-            this.stuffcost=stuffcost;
-        }
-        public void settotalcost(float totalcost)
-        {
-            this.totalcost=totalcost;
-        }
-    void display ()
-    {
-        System.out.println("Duration : " +duration);
-        System.out.println("Foodcost : " +foodcost);
-        System.out.println("Vanue : " +vanue);
-        System.out.println("Stuff cost : " +stuffcost);
-        System.out.println("Total cost : " +totalcost);
-    }
+public Expenditure(String duration, float foodcost, String vanue, float stuffcost, float totalcost,Event event) {
+    this.duration = duration;
+    this.foodcost = foodcost;
+    this.vanue = vanue;
+    this.stuffcost = stuffcost;
+    this.totalcost = totalcost;
+    this.event = event;
 }
+
+public String getDuration() {
+    return duration;
+}
+
+public void setDuration(String duration) {
+    this.duration = duration;
+}
+
+public float getFoodcost() {
+    return foodcost;
+}
+
+public void setFoodcost(float foodcost) {
+    this.foodcost = foodcost;
+}
+
+public String getVanue() {
+    return vanue;
+}
+
+public void setVanue(String vanue) {
+    this.vanue = vanue;
+}
+
+public float getStuffcost() {
+    return stuffcost;
+}
+
+public void setStuffcost(float stuffcost) {
+    this.stuffcost = stuffcost;
+}
+
+public float getTotalcost() {
+    return totalcost;
+}
+
+public void setTotalcost(float totalcost) {
+    this.totalcost = totalcost;
+}
+    
+    
+    void display ()
+{
+    event.display();
+    System.out.println("Duration : " +duration);
+    System.out.println("Foodcost : " +foodcost);
+    System.out.println("Vanue : " +vanue);
+    System.out.println("Stuff cost : " +stuffcost);
+    System.out.println("Total cost : " +totalcost);
+}
+    
+
+}
+
